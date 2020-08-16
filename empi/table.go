573ee0998d2150fa875fc67b69d4bc55b7dab400
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-08-16 10:28:32 -0700

    GatherTableRows ensures cols same on dest

--- empi/table.go
+++ empi/table.go
@@ -11,12 +11,16 @@ import (
 
 // GatherTableRows does an MPI AllGather on given src table data, gathering into dest.
 // dest will have np * src.Rows Rows, filled with each processor's data, in order.
-// dest must be a clone of src
+// dest must be a clone of src: if not same number of cols, will be configured from src.
 func GatherTableRows(dest, src *etable.Table, comm *mpi.Comm) {
 	sr := src.Rows
 	np := mpi.WorldSize()
 	dr := np * sr
-	dest.SetNumRows(dr)
+	if len(dest.Cols) != len(src.Cols) {
+		dest.SetFromSchema(src.Schema(), dr)
+	} else {
+		dest.SetNumRows(dr)
+	}
 	for ci, st := range src.Cols {
 		dt := dest.Cols[ci]
 		GatherTensorRows(dt, st, comm)
