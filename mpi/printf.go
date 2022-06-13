20648f358f5ced2cf56d7283df20e0f27d269b43
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-06-12 17:30:08 -0700

    add mpi.println, remove redundant time scope from looper functions

--- mpi/printf.go
+++ mpi/printf.go
@@ -9,7 +9,8 @@ import "fmt"
 // PrintAllProcs causes mpi.Printf to print on all processors -- otherwise just 0
 var PrintAllProcs = false
 
-// Printf does fmt.Printf only on the 0 rank node (though see PrintAllProcs to do all)
+// Printf does fmt.Printf only on the 0 rank node (see also AllPrintf to do all)
+// and PrintAllProcs var to override for debugging, and print all
 func Printf(fs string, pars ...interface{}) {
 	if !PrintAllProcs && WorldRank() > 0 {
 		return
@@ -25,4 +26,23 @@ func Printf(fs string, pars ...interface{}) {
 func AllPrintf(fs string, pars ...interface{}) {
 	fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
 	fmt.Printf(fs, pars...)
+}
+
+// Println does fmt.Println only on the 0 rank node (see also AllPrintln to do all)
+// and PrintAllProcs var to override for debugging, and print all
+func Println(fs string) {
+	if !PrintAllProcs && WorldRank() > 0 {
+		return
+	}
+	if WorldRank() > 0 {
+		fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
+	}
+	fmt.Println(fs)
+}
+
+// AllPrintln does fmt.Println on all nodes, with node rank printed first
+// This is best for debugging MPI itself.
+func AllPrintln(fs string) {
+	fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
+	fmt.Println(fs)
 }
