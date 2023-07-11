4d37da0cf0a35dd050728ef52ee37aad0787ac3f
Randall C. O'Reilly <oreilly@ucdavis.edu>
2023-07-11 13:18:37 -0700

    fix mpi.Println to use any varargs like fmt actually does

--- mpi/printf.go
+++ mpi/printf.go
@@ -11,38 +11,42 @@ var PrintAllProcs = false
 
 // Printf does fmt.Printf only on the 0 rank node (see also AllPrintf to do all)
 // and PrintAllProcs var to override for debugging, and print all
-func Printf(fs string, pars ...interface{}) {
+func Printf(fs string, pars ...any) {
 	if !PrintAllProcs && WorldRank() > 0 {
 		return
 	}
 	if WorldRank() > 0 {
-		fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
+		AllPrintf(fs, pars...)
+	} else {
+		fmt.Printf(fs, pars...)
 	}
-	fmt.Printf(fs, pars...)
 }
 
 // AllPrintf does fmt.Printf on all nodes, with node rank printed first
 // This is best for debugging MPI itself.
-func AllPrintf(fs string, pars ...interface{}) {
+func AllPrintf(fs string, pars ...any) {
 	fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
 	fmt.Printf(fs, pars...)
 }
 
 // Println does fmt.Println only on the 0 rank node (see also AllPrintln to do all)
 // and PrintAllProcs var to override for debugging, and print all
-func Println(fs string) {
+func Println(fs ...any) {
 	if !PrintAllProcs && WorldRank() > 0 {
 		return
 	}
 	if WorldRank() > 0 {
-		fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
+		AllPrintln(fs...)
+	} else {
+		fmt.Println(fs)
 	}
-	fmt.Println(fs)
 }
 
 // AllPrintln does fmt.Println on all nodes, with node rank printed first
 // This is best for debugging MPI itself.
-func AllPrintln(fs string) {
-	fs = fmt.Sprintf("P%d: ", WorldRank()) + fs
-	fmt.Println(fs)
+func AllPrintln(fs ...any) {
+	fsa := make([]any, len(fs))
+	copy(fsa[1:], fs)
+	fsa[0] = fmt.Sprintf("P%d: ", WorldRank())
+	fmt.Println(fsa...)
 }
